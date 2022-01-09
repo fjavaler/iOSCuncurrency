1ce4d6124c8def7433717206d350aa5882638f5d
Frederick Javalera <frederickjavalera@gmail.com>
2022-01-08 17:54:49 -0700

    Initial Commit

---
+++ iOSCuncurrency/iOSCuncurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCuncurrencyApp.swift
+//  iOSCuncurrency
+//
+//  Created by Fred Javalera on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSCuncurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
